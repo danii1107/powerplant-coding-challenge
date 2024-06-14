b8eb4ded3e6caedf89e3e8ea079e9094969ea299
danii1107 <danielbarsan72003@gmail.com>
2024-06-14 13:28:07 +0200

    new way to implement the algorithm explained in each function documentation, meritOrder has to do the comparisons and solve only switch on powerplants

--- unitCommitment.py
+++ unitCommitment.py
@@ -2,6 +2,12 @@ from models import Response, Payload, Powerplant
 
 def meritOrder(powerplants: list) -> list:
 	"""
+	Started ordering powerplants by efficiency, but it won't be enough to solve the unit commitment problem because we will need
+	to consider the cost and the % wind of each powerplant as well. A gasfired pp with 0.2 efficiency and 10 euro/MWh will be in a higher position than
+	a kerosine one with 0.3 efficiency and 50 euro/MWh, obviously you will preffer to switch on the gasfired one. The same goes for the windturbines, we will need
+	to consider the pmax of each to decide which one to switch on first and maybe don't swithc on 2 powerplant when we can reach the load with only 1 switched on.
+	So, we will need to add some conditions for the ordering.
+
 	Sorts the powerplants by its merit-order.
 
 	The merit-order is a way of ranking available sources of energy, especially electrical generation, based on ascending order of price (which may reflect 
@@ -11,7 +17,8 @@ def meritOrder(powerplants: list) -> list:
 
 def solve(payload: dict) -> list:
 	"""
-	Solves the unit commitment problem.
+	I thought about always siwtching on the windturbines while needed load > windturbin pmax*%wind and then switch on the powerplants in the merit order. But meritorder
+	is not well-implemented, the solve function should switch on powerplant on its merit order until the load is reached, we don't need extra comparisons here.
 	"""
 	response = []
 	sortedPowerplants = meritOrder(payload.powerplants)
